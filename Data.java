b805219c4eaeac262d9bc7f39f9718e07853e633
Willy-inovem <willy_marwinata_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:48:48 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,14 @@
+public class Data {
+    public static void main(String[] args) {
+        //tipe_data nama_var = value/isi var
+        String nama = "Willy sigma skibidi";
+        int usia = 15;
+        String Jalan = "Jalan jalan";
+        double kelas = 10.5;
+
+System.out.println("nama saya : " +  nama);
+System.out.println("usia saya :" + usia + " Abad");
+System.out.println("Jalan : " + Jalan);
+System.out.println("kelas : " + kelas);
+    }
+}
